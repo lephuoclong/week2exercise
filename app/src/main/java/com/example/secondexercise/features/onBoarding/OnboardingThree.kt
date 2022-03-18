fc7aa5763645d73b563a4f740f71bd543d54f158
thinh29 <thinh290800@gmail.com>
2022-03-18 18:25:49 +0700

    Week 2 Thinh

--- app/src/main/java/com/example/secondexercise/features/onBoarding/OnboardingThree.kt
+++ app/src/main/java/com/example/secondexercise/features/onBoarding/OnboardingThree.kt
@@ -1,12 +1,90 @@
 package com.example.secondexercise.features.onBoarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.Window
+import android.widget.Button
 import com.example.secondexercise.R
+import com.example.secondexercise.features.welcome.Welcome
+import kotlin.math.abs
 
-class OnboardingThree : AppCompatActivity() {
+@Suppress("DEPRECATION")
+
+class OnboardingThree : AppCompatActivity(), GestureDetector.OnGestureListener {
+    private var btnNext3: Button?=null
+    lateinit var gestureDetector: GestureDetector
+    private val swipeThreshold = 100
+    private val swipeVelocityThreshold = 100
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportActionBar?.hide();
         setContentView(R.layout.activity_onboarding_three)
+
+        init()
+
+        btnNext3?.setOnClickListener {
+            goToNextActivity()
+        }
+    }
+
+    private fun init(){
+        btnNext3 = findViewById(R.id.btn_next_3)
+    }
+
+    private fun goToNextActivity(){
+        var intent = Intent(this, Welcome::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else {
+            super.onTouchEvent(event)
+        }
+    }
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return false
+    }
+    override fun onShowPress(p0: MotionEvent?) {
+        return
+    }
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return false
+    }
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return false
+    }
+    override fun onLongPress(p0: MotionEvent?) {
+        return
+    }
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        try {
+            val diffY = e2.y - e1.y
+            val diffX = e2.x - e1.x
+            if (abs(diffX) > abs(diffY)) {
+                if (abs(diffX) > swipeThreshold && abs(velocityX) > swipeVelocityThreshold) {
+                    if (diffX > 0) {
+                        return false
+                    }
+                    else {
+                        goToNextActivity()
+                    }
+                }
+            }
+        }
+        catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+        return true
     }
 }
